81517d254a8f80a64fb7757beb5926ed342da5fe
kailandrewhamili <ahamili@ventaja.com.ph>
2017-07-28 17:05:53 +0800

    Chat Client

---
+++ Quiz/Window.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Window
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.cmbAddress = New System.Windows.Forms.ComboBox()
+        Me.txtmessage = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(40, 59)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(148, 177)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(40, 243)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 1
+        Me.btnSend.Text = "Button1"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(40, 23)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 26)
+        Me.txtName.TabIndex = 2
+        '
+        'cmbAddress
+        '
+        Me.cmbAddress.FormattingEnabled = True
+        Me.cmbAddress.Location = New System.Drawing.Point(166, 23)
+        Me.cmbAddress.Name = "cmbAddress"
+        Me.cmbAddress.Size = New System.Drawing.Size(121, 26)
+        Me.cmbAddress.TabIndex = 3
+        '
+        'txtmessage
+        '
+        Me.txtmessage.Location = New System.Drawing.Point(207, 59)
+        Me.txtmessage.Name = "txtmessage"
+        Me.txtmessage.Size = New System.Drawing.Size(100, 26)
+        Me.txtmessage.TabIndex = 4
+        '
+        'Window
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(426, 361)
+        Me.Controls.Add(Me.txtmessage)
+        Me.Controls.Add(Me.cmbAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Window"
+        Me.Text = "Window"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents cmbAddress As ComboBox
+    Friend WithEvents txtmessage As TextBox
+End Class
---
+++ Quiz/Window.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+Imports System.Net.Sockets
+Imports System.Threading
+
+Public Class Window
+    Dim Listener As New TcpListener(65535)
+    Dim Client As New TcpClient
+    Dim Message As String = ""
+
+    Dim ListenerThread As New Thread(New ThreadStart(AddressOf Listening))
+
+    Private Sub Listening()
+        Listener.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Listener.Pending = True Then
+            Message = ""
+            Client = Listener.AcceptTcpClient()
+
+            Dim Reader As New StreamReader(Client.GetStream())
+            While Reader.Peek > -1
+                Message = Message + Convert.ToChar(Reader.Read()).ToString
+            End While
+            With RichTextBox1
+                .ForeColor = Color.Black
+                .Text += Message + vbCrLf
+            End With
+        End If
+    End Sub
+    Private Sub btnSend_Click(ByVal sender As System.Object,
+            ByVal e As System.EventArgs) Handles btnSend.Click
+        If txtName.Text = "" Or cmbAddress.Text = "" Then
+            'Check to make sure that the user has entered 
+            'a display name, and a client IP Address
+            'If Not, Show a Message Box
+            MessageBox.Show("All Fields must be Filled",
+                            "Error Sending Message",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Try
+                Client = New TcpClient(cmbAddress.Text, 65535)
+
+                'Declare the Client as an IP Address. 
+                'Must be in the Correct form. eg. 000.0.0.0
+                Dim Writer As New StreamWriter(Client.GetStream())
+                Writer.Write(txtName.Text & " Says:  " & txtmessage.Text)
+                Writer.Flush()
+
+                'Write the Message in the stream
+
+                RichTextBox1.Text += (txtName.Text & " Says:  " & txtmessage.Text) + vbCrLf
+                txtmessage.Text = ""
+            Catch ex As Exception
+                Console.WriteLine(ex)
+                Dim Errorresult As String = ex.Message
+                MessageBox.Show(Errorresult & vbCrLf & vbCrLf &
+                                "Please Review Client Address",
+                                "Error Sending Message",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub Window_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Listening()
+        Timer1.Enabled = True
+    End Sub
+End Class
